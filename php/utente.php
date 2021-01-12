e2623e0e89d615f8fbe6d20168192f3d284802ac
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-12 10:11:01 +0100

    Riabilitate pagine utente e registrazione

--- php/utente.php
+++ php/utente.php
@@ -1,17 +1,17 @@
 <?php
     require_once __DIR__ . "/setterTemplate.php";
 
-    $setterPagina = new setterTemplate("../");
+    $setterPagina = new setterTemplate("..");
 
-    /*manca codice per settare i meta, da inserire nella classe in setterTemplate*/
+    $setterPagina->setTitle("Utente | The Darksoulers");
+    $setterPagina->setDescription("Pagina iniziale del sito The Darksoulers");  
 
-    $setterPagina->setHeader();
+    $setterPagina->setNavBar(file_get_contents(__DIR__ . "/contents/home-nav.php"));
+
+    $setterPagina->setPercorso("Utente");
     
     $setterPagina->setContent("utenteContent.php");
     $setterPagina->setFooter();
 
-    $setterPagina->sistemaLink();
-    $setterPagina->sistemaMenu();
-    $setterPagina->stampaHtml(); //fa l'echo della pagina
-
+    $setterPagina->validate();
 ?>
