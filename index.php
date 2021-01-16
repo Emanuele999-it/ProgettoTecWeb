403835d325ebec30f119c1b5178031ca8527f8b6
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-16 18:00:12 +0100

    risolto problema

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__ . "/php/setterTemplate.php";
-    require_once __DIR__ . "/query-lista-articoli.php";
+    require_once __DIR__ . "/php/query-lista-articoli.php";
 
     $setterPagina = new setterTemplate(".");
 
