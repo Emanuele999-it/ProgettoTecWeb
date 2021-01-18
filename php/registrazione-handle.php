694947a1495cca11ba865f40435afb279dd861bd
Davide <sgrazzuttidavide@gmail.com>
2021-01-18 18:20:07 +0100

    prova registrazione

--- php/registrazione-handle.php
+++ php/registrazione-handle.php
@@ -14,10 +14,10 @@ $_SESSION["password"]   =   $_POST['password1'];
 
 
 $connection = new DBConnection();
-$queryperincremento= query("SELECT COUNT(nome) FROM utente");
-$queryperincremento++;
+/*$queryperincremento= query("SELECT COUNT(nome) FROM utente");
+$queryperincremento++;*/
 $query      = "INSERT INTO utente (nome,cognome,email,img_path,passw) 
-                VALUES ('prova','prova','$queryperincremento','prova','prova')";
+                VALUES ('prova','prova','email di prova','prova','prova')";
 
 $connection->query($query);
 
