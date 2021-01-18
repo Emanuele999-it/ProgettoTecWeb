44f9381c8890ec6cb319717b22fa688d09722c5f
Davide <sgrazzuttidavide@gmail.com>
2021-01-18 17:37:15 +0100

    222

--- php/registrazione-handle.php
+++ php/registrazione-handle.php
@@ -17,7 +17,7 @@ $connection = new DBConnection();
 $queryperincremento="SELECT COUNT(nome) FROM utente";
 $queryperincremento++;
 $query      = "INSERT INTO utente (nome,cognome,email,img_path,passw) 
-                VALUES ('prova','prova','$queryperincremento','prova','prova')";
+                VALUES ('prova','prova',"$queryperincremento",'prova','prova')";
 
 $connection->query($query);
 
