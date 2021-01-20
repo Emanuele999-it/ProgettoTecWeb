45e85779cb7e86e82cc341e3c6cc4ed09763f959
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-20 20:29:49 +0100

    Revert "Risolto problema path"
    This reverts commit 1c80522db887004079344327863463577383853f.
    Revert HEAD~8

--- php/menu.php
+++ php/menu.php
@@ -88,7 +88,7 @@ $setterPagina->setNavBar($nav);
 
 
 //controllo se l'utente e' loggato
-$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"), file_get_contents(__DIR__ . "/contents/logRegMobileContent.php"));
+$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/php/contents/logRegContent.php"), file_get_contents(__DIR__ . "/php/contents/logRegMobileContent.php"));
 
 
 $setterPagina->setFooter();
