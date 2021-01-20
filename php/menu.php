e2fbbd91fba53640b14a6fe5af4131217e1c89fd
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-20 20:27:14 +0100

    ennesimo commit pre revert

--- php/menu.php
+++ php/menu.php
@@ -4,6 +4,8 @@ require_once __DIR__ . "/query-articoli.php";
 
 $setterPagina = new setterTemplate("..");
 
+//controllo se l'utente e' loggato
+$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/php/contents/logRegContent.php"), file_get_contents(__DIR__ . "/php/contents/logRegMobileContent.php"));
 
 if (key_exists("pagina", $_GET)) {
     if (intval($_GET["pagina"] < 1)) {
@@ -84,9 +86,10 @@ if ($currPag != 0) {
 
 $setterPagina->setNavBar($nav);
 
+
 //controllo se l'utente e' loggato
 $setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"), file_get_contents(__DIR__ . "/contents/logRegMobileContent.php"));
 
-
+// parent of f83b0d3 (fix accedi/registrati)
 $setterPagina->setFooter();
 $setterPagina->validate();
