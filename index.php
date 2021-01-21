448d304f2f0113440abba0a4a07018f9ec58ea26
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-21 16:23:14 +0100

    index no accedi/registrati

--- index.php
+++ index.php
@@ -24,15 +24,15 @@
 
 
 	//controllo se l'utente e' loggato
-	
-	if ((isset($_SESSION['loggedin']) && $_SESSION['loggedin'])) {
-		$utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLoginMobile.php"));
-		$utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLogin.php"));
-		$setterPagina->setLoginContent($utenteFull,$utenteMobile);
+	if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'])) {
+		$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"),file_get_contents(__DIR__ . "/contents/logRegMobileContent.php") );
 	}
 	else {
-		$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"), file_get_contents(__DIR__ . "/contents/logRegMobileContent.php"));	
+		$utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLoginMobile.php"));
+		$utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLogin.php"));
+		$setterPagina->setLoginContent($utenteFull, $utenteMobile);
 	}
+	
 	
 
     $last3articoli = getArticoli(0, 3, true);
