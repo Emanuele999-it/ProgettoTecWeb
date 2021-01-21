1a41aebe777b19a37fd5a0fc839091435dd7528f
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-21 15:54:58 +0100

    sessione articoli

--- php/articolo.php
+++ php/articolo.php
@@ -1,37 +1,47 @@
 <?php
-require_once __DIR__ . "/setterTemplate.php";
-require_once __DIR__ . "/query-articoli.php";
+	require_once __DIR__ . "/setterTemplate.php";
+	require_once __DIR__ . "/query-articoli.php";
 
-$setterPagina = new setterTemplate("..");
+	$setterPagina = new setterTemplate("..");
 
-//controllo se l'utente e' loggato
-$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/php/contents/logRegContent.php"), file_get_contents(__DIR__ . "/php/contents/logRegMobileContent.php"));
+	//controllo se l'utente e' loggato
+	$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/php/contents/logRegContent.php"), file_get_contents(__DIR__ . "/php/contents/logRegMobileContent.php"));
 
-$setterPagina->setTitle("Articolo | The Darksoulers");
-$setterPagina->setDescription("Articolo riguardante un gioco");  
-$setterPagina->setNavBar(file_get_contents(__DIR__ . "/contents/home-nav.php"));
+	$setterPagina->setTitle("Articolo | The Darksoulers");
+	$setterPagina->setDescription("Articolo riguardante un gioco");  
+	$setterPagina->setNavBar(file_get_contents(__DIR__ . "/contents/home-nav.php"));
 
-$idArticolo = array_key_exists('id', $_GET) ? $_GET['id'] : 0;
+	$idArticolo = array_key_exists('id', $_GET) ? $_GET['id'] : 0;
 
-if ($idArticolo <= 0) {
-    header("Location: error/400.php");
-    exit;
-}
-$htmlArticolo = getSingoloArticolo($idArticolo);
-if ($htmlArticolo < 0) {
-    header("Location: error/404.php");
-    exit;
-}
+	if ($idArticolo <= 0) {
+		header("Location: error/400.php");
+		exit;
+	}
+	$htmlArticolo = getSingoloArticolo($idArticolo);
+	if ($htmlArticolo < 0) {
+		header("Location: error/404.php");
+		exit;
+	}
 
-$titoloArticolo = getTitolo($idArticolo);
-$setterPagina->setPercorso("Articoli -> $titoloArticolo");
+	$titoloArticolo = getTitolo($idArticolo);
+	$setterPagina->setPercorso("Articoli -> $titoloArticolo");
 
-//modifico il contenuto in base alla query ricevuta
-$artPageCon = file_get_contents(__DIR__ . "/contents/articolopageContent.php");
-$artPageCon = str_replace("<articoloContent />", $htmlArticolo, $artPageCon);
-$setterPagina->setContent($artPageCon);
-$setterPagina->setFooter();
+	//controllo se l'utente e' loggato
+	if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'])) {
+		$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"),file_get_contents(__DIR__ . "/contents/logRegMobileContent.php") );
+	}
+	else {
+		$utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLoginMobile.php"));
+		$utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLogin.php"));
+		$setterPagina->setLoginContent($utenteFull, $utenteMobile);
+	}
 
-$setterPagina->validate()
+	//modifico il contenuto in base alla query ricevuta
+	$artPageCon = file_get_contents(__DIR__ . "/contents/articolopageContent.php");
+	$artPageCon = str_replace("<articoloContent />", $htmlArticolo, $artPageCon);
+	$setterPagina->setContent($artPageCon);
+	$setterPagina->setFooter();
+
+	$setterPagina->validate()
 
 ?>
