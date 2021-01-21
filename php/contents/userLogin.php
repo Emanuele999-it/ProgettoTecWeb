1698ac1d65d393597aef4879fdceb43557366220
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-21 11:18:11 +0100

    commit prova userLogin

--- php/contents/userLogin.php
+++ php/contents/userLogin.php
@@ -1,4 +1,4 @@
 <ul id="AccediRegistrati">
-    <li class="elementomenu"><a  href="<rootFolder />/php/utente.php"><SegnapostoNome /></a>
+    <li class="elementomenu"><a  href="<rootFolder />/php/utente.php">SegnapostoNome </a>
     <li class="elementomenu"><a  href="<rootFolder />/php/handle-logout.php">Esci</a>
 </ul>
