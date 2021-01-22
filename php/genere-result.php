05fe2356fa3f5c1321ac1cc562c26191125994ab
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-22 15:45:54 +0100

    fix sessioni in aericoli e risultato generi

--- php/genere-result.php
+++ php/genere-result.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/setterTemplate.php";
 require_once __DIR__ . "/query-articoli.php";
+require_once __DIR__ . "/utente-class.php";
 
 $setterPagina = new setterTemplate("..");
 
@@ -75,13 +76,13 @@ if ($currCat != 0) {
 $setterPagina->setNavBar($nav);
 
 //controllo se l'utente e' loggato
-	if (!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'])) {
-		$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"),file_get_contents(__DIR__ . "/contents/logRegMobileContent.php") );
-	}
-	else {
+	if ($_SESSION['loggedin']) {
 		$utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLoginMobile.php"));
 		$utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLogin.php"));
 		$setterPagina->setLoginContent($utenteFull, $utenteMobile);
+	}
+	else{
+		$setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"),file_get_contents(__DIR__ . "/contents/logRegMobileContent.php") );
 	}
 
 $setterPagina->setFooter();
