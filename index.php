82112b1a3ab4a1fe3800d45ceb2e8a3d5dfd1531
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-22 11:16:11 +0100

    fix index sessione

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 		$utenteMobile = file_get_contents(__DIR__ . "/php/contents/userLoginMobile.php");
 		$utenteFull = file_get_contents(__DIR__ . "/php/contents/userLogin.php");
 		
-		$utenteMobile = str_replace("<a href=\"<rootFolder />/php/utente.php\"><SegnapostoNomeMobile /></a>","<SegnapostoNomeMobile />", file_get_contents(__DIR__ . "/php/contents/userLoginMobile.php"));
-		$utenteFull = str_replace("<a  href=\"<rootFolder />/php/utente.php\"><SegnapostoNome /></a>","<SegnapostoNome />", file_get_contents(__DIR__ . "/php/contents/userLogin.php"));
+		$utenteMobile = str_replace("<a href=\"<rootFolder />/php/utente.php\"><SegnapostoNomeMobile /></a>","<SegnapostoNomeMobile />", $utenteMobile);
+		$utenteFull = str_replace("<a  href=\"<rootFolder />/php/utente.php\"><SegnapostoNome /></a>","<SegnapostoNome />", $utenteFull);
 		
 		$utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/php/contents/userLoginMobile.php"));
 		$utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/php/contents/userLogin.php"));
