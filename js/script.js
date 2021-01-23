aef36ac53a95455e09c7d98d26a96e1528437328
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-23 21:42:13 +0100

    sistemato js

--- js/script.js
+++ js/script.js
@@ -48,11 +48,8 @@ function checkCommento(commento) {
 
 function loginCheck(user) {    
     var email = document.getElementById(user).value;
-    alert("email");
     var result = !(email === "");
-    alert("result");
-    notifyError(result, email, "Inserito campo vuoto");
-    alert("notify error");
+    notifyError(result, user, "Inserito campo vuoto");
 	return result;
 }
 
@@ -120,14 +117,10 @@ if (regist) {
 var login = document.getElementById("form-accesso");
 if (login) {
 	login.addEventListener("submit", function (event) {
-        alert("login");
 		if (!(loginCheck("login-email"))) {
-            alert("if");
             event.stopImmediatePropagation();
             event.stopPropagation();
             event.preventDefault();
         }
-        
-    alert("endoif");
 	});
 }
