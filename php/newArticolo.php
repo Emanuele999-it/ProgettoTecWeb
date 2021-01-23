f7a7b3f2e8e9e2b66e6cd66eb517ec7b2cf5e4a6
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-23 19:59:49 +0100

    fix newArticolo

--- php/newArticolo.php
+++ php/newArticolo.php
@@ -20,6 +20,15 @@
 	
     $setterPagina->setPercorso("Utente");
     
+    if ($_SESSION['loggedin']) {
+        $utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLoginMobile.php"));
+        $utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), file_get_contents(__DIR__ . "/contents/userLogin.php"));
+        $setterPagina->setLoginContent($utenteFull, $utenteMobile);
+        }
+    else{
+        $setterPagina->setLoginContent(file_get_contents(__DIR__ . "/contents/logRegContent.php"),file_get_contents(__DIR__ . "/contents/logRegMobileContent.php") );
+        }
+
     $newArtCon = file_get_contents(__DIR__ . "/contents/newArticoloContent.php");
     $setterPagina->setContent($newArtCon);
     $setterPagina->setFooter();
