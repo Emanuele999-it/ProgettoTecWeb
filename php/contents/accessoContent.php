34903db7df7a379abda016208581e7167e4f60f1
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-23 20:44:43 +0100

    aggiunti field in accesso

--- php/contents/accessoContent.php
+++ php/contents/accessoContent.php
@@ -8,10 +8,12 @@
                 <li class="form-element">
                     <label class="form-label" for="login-email"><span xml:lang="en" lang="en">Email</span>:</label>
                     <input id="login-email" class="inputNick" type="text" name="email" />
+                    <span id="login-email-warning" class="warning"></span>
                 </li>
                 <li class="form-element">
                     <label class="form-label" for="login-password"><span xml:lang="en" lang="en">Password</span>:</label>
                     <input id="login-password" class="inputPW" type="password" name="password" />
+                    <span id="login-password-warning" class="warning"></span>
                 </li>
                 <li class="form-element">
                     <input class="submit" type="submit" value="Accedi"/>
