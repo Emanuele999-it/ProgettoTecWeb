a21e9e70585862205a5a2bc0a3d1900f509b0b29
davidesgrazzutti <59489672+davidesgrazzutti@users.noreply.github.com>
2021-01-23 01:49:17 +0100

    Update articolo.php

--- php/articolo.php
+++ php/articolo.php
@@ -59,6 +59,31 @@
 		$artPageCon = str_replace("<SegnapostoDeleteArticolo />", "", $artPageCon);
 	}		
 
+       function commentiutenti ($idarticolo) {
+        $commenti = $connection->query("SELECT userid,testo,data_com 
+                                      FROM commento WHERE articolo_id = \"{$idarticolo}\"   ");
+        while ($row = $commenti->fetch_assoc()) {
+        $boxcommento = $boxcommento . file_get_contents(__DIR__ . "/contents/box-commento.php");
+        $inseriticommenti =  $row["testo"];
+        $datacommento = $row["data_com"];
+        $boxcommento = str_replace("<Segnapostotestocommento />",$inseriticommenti,$boxcommento);
+        $boxcommento = str_replace("<SegnapostoDatacommento />",$datacommento,$boxcommento);    
+
+        $utente      = $connection->query("SELECT nome FROM utente WHERE userid = \"{$row["userid"]}\"   ");
+        $row2        = $utente->fetch_assoc();
+        $prova       = $row2["nome"];
+        $boxcommento = str_replace("<SegnaPostonomeutente />",$prova,$boxcommento);     
+//ATTENZIONE CHE CI POSSONO ESSERE ARTICOLI SENZA COMMENTI
+        if (!$commenti) {
+            throw new Exception("commenti query sbagliata", 1);  //ATTENZIONE CHE CI POSSONO ESSERE ARTICOLI SENZA COMMENTI
+            exit;
+        } 
+        }
+
+       
+        return $boxcommento;
+    }
+
 	$id = $_GET["id"];
 	if ($_SESSION['loggedin']) {
 		$boxinsertcommento = file_get_contents(__DIR__ . "/contents/box-insert-commento.php");
