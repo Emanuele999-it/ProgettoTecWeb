aa0cf1fbabcabf2c02cb64c10005434337282e12
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-23 21:54:05 +0100

    sistemato hamburger menu accedi

--- php/accesso.php
+++ php/accesso.php
@@ -8,19 +8,15 @@
     $setterPagina->setDescription("Pagina di login");  
     $setterPagina->setPercorso("Accedi");
 
-    $setterPagina->setNavBar(
-        preg_replace(
-            "((?s)<a href=\"<rootFolder />/php/accesso.php\">Accedi</a>)",
-            "<a href=\"#header\" class=\"active\">Accedi</a>",
-            preg_replace(
-                "((?s)<li class=\"elementomenu\"><a href=\"<rootFolder />/php/accesso.php\">Accedi</a></li>)",
-                "<li id=\"currentLink\" class=\"elementomenu\">Accedi</li>",
-                file_get_contents(__DIR__ . "/contents/home-nav.php")))
-	);
+    $setterPagina->setNavBar(file_get_contents(__DIR__ . "/contents/home-nav.php"));
 
     //accesso
-	$utenteMobile = file_get_contents(__DIR__ . "/contents/logRegMobileContent.php");
-	$utenteFull = file_get_contents(__DIR__ . "/contents/logRegContent.php");
+	$utenteMobile = preg_replace(
+        "((?s)<a href=\"<rootFolder />/php/accesso.php\">Accedi</a>)",
+        "<a href=\"#header\" class=\"active\">Accedi</a>",file_get_contents(__DIR__ . "/contents/logRegMobileContent.php"));
+	$utenteFull = preg_replace(
+        "((?s)<li class=\"elementomenu\"><a href=\"<rootFolder />/php/accesso.php\">Accedi</a></li>)",
+        "<li id=\"currentLink\" class=\"elementomenu\">Accedi</li>",file_get_contents(__DIR__ . "/contents/logRegContent.php"));
 		
 	$utenteMobile = str_replace("<a href=\"<rootFolder />/php/accesso.php\">Accedi</a>","Accedi", $utenteMobile);
 	$utenteFull = str_replace("<a href=\"<rootFolder />/php/accesso.php\">Accedi</a></li>","Accedi", $utenteFull);
