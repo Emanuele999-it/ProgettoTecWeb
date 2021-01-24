5220d8923d03375b64e2ac6c0c79df1f6a55a71c
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-24 18:50:23 +0100

    aggiornato errore 401

--- php/error/401.php
+++ php/error/401.php
@@ -1,1 +1,33 @@
-test 401
+<?php
+require_once __DIR__ . "/../setterTemplate.php";
+require_once __DIR__ . "/../utente-class.php";
+
+session_start();
+
+$setterPagina = new setterTemplate("../..");
+
+$setterPagina->setTitle("The Darksoulers");
+$setterPagina->setDescription("Pagina iniziale errore 401");
+
+$setterPagina->setPercorso("Errore 401");
+
+$setterPagina->setNavBar(file_get_contents(__DIR__ . "/../contents/home-nav.php"));
+
+
+//controllo se l'utente e' loggato
+if ($_SESSION['loggedin']){
+    $utenteMobile = file_get_contents(__DIR__ . "/../contents/userLoginMobile.php");
+    $utenteFull = file_get_contents(__DIR__ . "/../contents/userLogin.php");
+    
+    $utenteMobile = str_replace("<SegnapostoNomeMobile />", $_SESSION['user']->getNome(), $utenteMobile);
+    $utenteFull = str_replace("<SegnapostoNome />", $_SESSION['user']->getNome(), $utenteFull);
+    $setterPagina->setLoginContent($utenteFull, $utenteMobile);
+}
+else {
+    $setterPagina->setLoginContent(file_get_contents(__DIR__ . "/../contents/logRegContent.php"),file_get_contents(__DIR__ . "/../contents/logRegMobileContent.php") );
+}
+
+$setterPagina->setContent("<h1>Ops.. sembra che non puoi accedere a questa risorsa</h1>");
+$setterPagina->setFooter();
+
+$setterPagina->validate();
