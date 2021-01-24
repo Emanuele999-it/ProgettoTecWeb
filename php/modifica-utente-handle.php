748eab9de7303719b34093e46414a69a6c4ce70b
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-24 11:41:12 +0100

    test

--- php/modifica-utente-handle.php
+++ php/modifica-utente-handle.php
@@ -5,11 +5,13 @@ require_once __DIR__ . "/utente-class.php";
 session_start();
 
 $connection = new DBConnection();
-$connection->query("UPDATE utente  SET (nome=\"{$_POST["nome"]}\", cognome=\"{$_POST["cognome"]}\", email=\"{$_POST["email"]}\", passw=\"{$_POST["password"]}\") WHERE userid=\"{$_SESSION["user"]->getUserid()}\"");
-$connection->disconnect();
-$_SESSION['user']->update();
-header("Location: ../php/utente.php");
-exit;
 
+$result = $connection->query("UPDATE utente  SET (nome=\"{$_POST["nome"]}\", cognome=\"{$_POST["cognome"]}\", email=\"{$_POST["email"]}\", passw=\"{$_POST["password"]}\") 
+WHERE userid=\"{$_SESSION["user"]->getUserid()}\"");
+if(!$result) {
+    throw new Exception("update utente rip",1);
+    exit;
+}
+$connection->disconnect();
 $_SESSION['user']->update();
 header("Location: ../php/utente.php");
