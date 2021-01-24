05c3ee2507b4fe858af078a91a2194d365c71655
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-24 13:33:26 +0100

    modifica contenuto pagina articolo

--- php/contents/articolopageContent.php
+++ php/contents/articolopageContent.php
@@ -1,6 +1,8 @@
 <div id="contenutoArticoli" class="contenutoGenerale" >
     <articoloContent />    
     <SegnapostoDeleteArticolo />
+	<br>
+	<segnapostoVoto />
     <div class="torna-su" >
         <a class="torna-su-link" href="#header">Torna su</a>
     </div>
