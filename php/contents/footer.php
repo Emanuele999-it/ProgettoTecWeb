c85abd10d2e7b03730199140f090c6f46644210f
FilippoRuff <filipporuffin@libero.it>
2021-01-24 21:17:35 +0100

    Riempito footer

--- php/contents/footer.php
+++ php/contents/footer.php
@@ -1,5 +1,12 @@
 <div id="footer">
-    <img id="imgValidCode" src="img/valid-xhtml10.png" alt="" style="float:left;" />
-    <img id="imgValidCss" src="img/vcss-blue.gif" alt="" style="float:left;" />
-    <p>Testo visibile del footer.</p>
+    <img id="imgValidCode" src="img/valid-xhtml10.png" alt="" />
+    <p>The Darksoulers</p>
+    <p>Creators:</p>
+    <ul>
+        <li>Ruffin Filippo</li>
+        <li>Sgrazzutti Davide</li>
+        <li>Bertoldo Damiano</li>
+        <li>Emanuele Pase</li>
+    </ul>
+    <img id="imgValidCss" src="img/vcss-blue.gif" alt="" />
 </div>
