3188842437a69c2c29af9370f224142a123132e9
FilippoRuff <filipporuffin@libero.it>
2021-01-24 23:03:36 +0100

    Modifica footer

--- php/contents/footer.php
+++ php/contents/footer.php
@@ -1,12 +1,6 @@
 <div id="footer">
     <img id="imgValidCode" src="img/valid-xhtml10.png" alt="Certificato xhtml valido" />
     <p>The Darksoulers</p>
-    <p>Creators:</p>
-    <ul>
-        <li>Ruffin Filippo</li>
-        <li>Sgrazzutti Davide</li>
-        <li>Bertoldo Damiano</li>
-        <li>Emanuele Pase</li>
-    </ul>
+    <p>Creators: Ruffin Filippo, Sgrazzutti Davide, Bertoldo Damiano, Emanuele Pase.</p>
     <img id="imgValidCss" src="img/vcss-blue.gif" alt="Certificato css valido" />
 </div>
