67b4371865c9ff83f2cf51ba22c1f896032d462c
Emanuele999-it <62944317+Emanuele999-it@users.noreply.github.com>
2021-01-24 16:44:38 +0100

    Update accediORegistratiVoto.php

--- php/contents/accediORegistratiVoto.php
+++ php/contents/accediORegistratiVoto.php
@@ -1,3 +1,3 @@
 <p class="tasti-voto-avviso">
-	<a href="<rootFolder />/php/accesso.php"> Accedi</a> o <a href="<rootFolder />/php/registrazione.php">registrati</a> per votare questa ricetta.
+	<a href="<rootFolder />/php/accesso.php"> Accedi</a> o <a href="<rootFolder />/php/registrazione.php">registrati</a> per dare una valutazione al gioco.
 </p>
