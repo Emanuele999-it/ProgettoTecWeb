941f96fc6246a0e3b1f3722b91fc11f752d76d4a
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-24 23:10:10 +0100

    fix footer

--- php/contents/footer.php
+++ php/contents/footer.php
@@ -1,5 +1,4 @@
 <div id="footer">
-    <img id="imgValidCode" src="img/valid-xhtml10.png" alt="Certificato xhtml valido" />
     <p>The Darksoulers</p>
     <p>Creators:</p>
     <ul>
@@ -8,5 +7,6 @@
         <li>Bertoldo Damiano</li>
         <li>Emanuele Pase</li>
     </ul>
+	<img id="imgValidCode" src="img/valid-xhtml10.png" alt="Certificato xhtml valido" />
     <img id="imgValidCss" src="img/vcss-blue.gif" alt="Certificato css valido" />
 </div>
