dec8f45b02e1fbddcf26a63d5efd422a1dc83e0f
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-24 23:15:10 +0100

    fix footer dopo merge

--- php/contents/footer.php
+++ php/contents/footer.php
@@ -1,16 +1,6 @@
 <div id="footer">
     <p>The Darksoulers</p>
-<<<<<<< HEAD
-    <p>Creators:</p>
-    <ul>
-        <li>Ruffin Filippo</li>
-        <li>Sgrazzutti Davide</li>
-        <li>Bertoldo Damiano</li>
-        <li>Emanuele Pase</li>
-    </ul>
-	<img id="imgValidCode" src="img/valid-xhtml10.png" alt="Certificato xhtml valido" />
-=======
     <p>Creators: Ruffin Filippo, Sgrazzutti Davide, Bertoldo Damiano, Emanuele Pase.</p>
->>>>>>> 8664678a607d887e2238e803f9c97885e61d8dd2
+	<img id="imgValidCode" src="img/valid-xhtml10.png" alt="Certificato xhtml valido" />
     <img id="imgValidCss" src="img/vcss-blue.gif" alt="Certificato css valido" />
 </div>
