e43eacac29931284af09b72130c03a78da382652
Emanuele999-it <emanuele.pase99@gmail.com>
2021-01-24 16:20:58 +0100

    prova fix elimina-voto

---
+++ php/elimina-voto.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . "/db-handler.php";
+require_once __DIR__ . "/utente-class.php";
+
+session_start();
+
+$connection = new DBConnection();
+
+if (!key_exists("user", $_SESSION)) {
+    header("Location: ../error/401.php");
+	exit;
+}
+
+$id = $_SESSION['user']->getUserid();
+$GameId = $_GET['idgame'];
+$idarticolo = $_GET['idArticolo'];
+
+/*$result = $connection->query("SELECT voto FROM voto WHERE gioco_id=$GameId AND userid=$id");
+if (!$result) {
+    header("Location: ../error/402.php");
+	exit;
+}*/
+
+$result=$connection->query("DELETE FROM voto WHERE gioco_id=$GameId AND userid=$id");
+if (!$result) {
+	throw new Exception("prova stampa",1);
+    //header("Location: ../error/402.php");
+	exit;
+}
+$connection->disconnect();
+
+
+header("Location: articolo.php?id=$idarticolo");
+
+
+?>
