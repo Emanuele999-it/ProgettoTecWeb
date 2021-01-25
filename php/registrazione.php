585cbe24c9153bc8ce2be39b3c8c272090a30591
ToldoDM <bertoldo.damiano@gmail.com>
2021-01-25 10:10:13 +0100

    sistemata parte registrazione

--- php/registrazione.php
+++ php/registrazione.php
@@ -17,17 +17,7 @@ $setterPagina->setTitle("Registrati | The Darksoulers");
 $setterPagina->setDescription("Pagina per la registrazione");
 
 
-$setterPagina->setNavBar(
-    preg_replace(
-        "((?s)<a href=\"<rootFolder />/php/registrazione.php\">Registrati</a>)",
-        "<a href=\"#header\" class=\"active\">Registrati</a>",
-        preg_replace(
-            "((?s)<li class=\"elementomenu\"><a href=\"<rootFolder />/php/registrazione.php\">Registrati</a></li>)",
-            "<li id=\"currentLink\" class=\"elementomenu\">Registrati</li>",
-            file_get_contents(__DIR__ . "/contents/home-nav.php")
-        )
-    )
-);
+$setterPagina->setNavBar(file_get_contents(__DIR__ . "/contents/home-nav.php"));
 
 $setterPagina->setPercorso("Registrati");
 
