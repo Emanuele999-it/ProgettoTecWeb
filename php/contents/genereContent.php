7b3a3dbc1b33f4b1109e716910b44923aa5c08cc
FilippoRuff <filipporuffin@libero.it>
2021-01-25 09:33:14 +0100

    validazione genere content - link

--- php/contents/genereContent.php
+++ php/contents/genereContent.php
@@ -1,27 +1,27 @@
 <div  class="contenutoGenerale" >
     <h3>Seleziona il genere di gioco che preferisci<br/>per avere le ultime notizie</h3>
-    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=1&page=0" class="link-box-genere">
+    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=1&amp;page=0" class="link-box-genere">
         Giochi di ruolo</a></h4><img class="immagine-box"
         src="../img/rpg.jpg" alt=""/>
     </div>
-    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=2&page=0" class="link-box-genere">
+    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=2&amp;page=0" class="link-box-genere">
         Sportivi</a></h4>
         <img class="immagine-box"
         src="../img/sport.jpg" alt=""/>
     </div>
-    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=3&page=0" class="link-box-genere">
+    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=3&amp;page=0" class="link-box-genere">
         Sparatutto</a></h4><img class="immagine-box"
         src="../img/fps.jpg" alt=""/>
     </div>
-    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=4&page=0" class="link-box-genere">
+    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=4&amp;page=0" class="link-box-genere">
         Avventura</a></h4><img class="immagine-box"
         src="../img/avventura.jpg" alt=""/>
     </div>
-    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=5&page=0" class="link-box-genere">
+    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=5&amp;page=0" class="link-box-genere">
         Azione</a></h4><img class="immagine-box"
         src="../img/azione.jpg" alt=""/>
     </div>
-    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=6&page=0" class="link-box-genere">
+    <div class="boxgenere"><h4><a href="<rootFolder />/php/genere-result.php?id=6&amp;page=0" class="link-box-genere">
         Gestionale</a></h4><img class="immagine-box"
         src="../img/gestionale.jpg" alt=""/>
     </div>
